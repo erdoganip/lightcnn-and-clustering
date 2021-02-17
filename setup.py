a13ed8ac4b17b74536393fd55d31382a3f75495f
İpek Erdoğan <32425967+erdoganip@users.noreply.github.com>
2021-02-17 22:24:21 +0300

    setup and visualize

---
+++ setup.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import (
+    setup,
+    find_packages)
+
+runtime_packages = [
+"Click==7.0",
+"cycler==0.10.0",
+"Flask==1.1.1",
+"itsdangerous==1.1.0",
+"Jinja2==2.10.1",
+"kiwisolver==1.1.0",
+"MarkupSafe==1.1.1",
+"matplotlib==3.1.1",
+"numpy==1.20.1",
+"pandas==1.2.2",
+"profilehooks==1.11.0",
+"pyparsing==2.4.1.1",
+"python-dateutil==2.8.0",
+"pytz==2019.1",
+"scipy==1.6.0",
+"six==1.12.0",
+"Werkzeug==0.15.5",
+"pyflann-py3==0.1.0"
+]
+
+setup(
+    name='approximate-nn-clustering',
+    version='0.0.2',
+    description='A modified version of rank order clustering',
+    author='Varun Suresh',
+    author_email='fab.varun@gmail.com',
+    license='MIT',
+    packages=find_packages(),
+    install_requires=runtime_packages,
+    )
---
+++ visualize.py
@@ -0,0 +1,74 @@
+"""
+Flask app to visualize clusters.
+"""
+import os
+import json
+import argparse
+from flask import Flask, render_template, request, send_from_directory
+
+app = Flask(__name__, static_url_path='')
+
+
+def chunks(clusters, size):
+    """
+    Split clusters into chunks of size=size
+    """
+    for i in range(0, len(clusters), size):
+        yield clusters[i:i+size]
+
+def create_idx_to_path(lfw_dir):
+    """
+    Create a dictionary where the key is the image index and the value is the
+    path to the image.
+    """
+    labels = sorted(os.listdir(lfw_dir))
+    labels = [f for f in labels if not (f.startswith("."))]
+    idx = 0
+    idx_to_path = {}
+    for label in labels:
+        for img in sorted(os.listdir(os.path.join(lfw_dir, label))):
+            idx_to_path[idx] = os.path.join(lfw_dir, label, img)
+            idx += 1
+    return idx_to_path
+
+@app.route('/img/<path:fpath>', methods=['GET'])
+def get_img_path(fpath):
+    print(os.path.dirname(fpath), fpath.split('/')[-1])
+    return send_from_directory(os.path.dirname(fpath),fpath.split('/')[-1])
+
+@app.route('/single_cluster', methods=["GET"])
+def display_one_cluster():
+    """
+    Method to display images in ine cluster.
+    """
+    cluster_id = request.args.get('cluster')
+    return render_template("single_cluster.html",
+                           cluster_id=cluster_id,
+                           idx_to_path=idx_to_path,
+                           clusters=clusters)
+
+
+@app.route('/clusters', methods=["GET"])
+def display_clusters():
+    """
+    Method to display the clusters
+    """
+    offset = int(request.args.get('offset', '0'))
+    limit = int(request.args.get('limit', '50'))
+    clusters_id_sorted = sorted(clusters, key=lambda x : -len(clusters[x]))
+    batches = chunks(range(len(clusters_id_sorted)), size=limit)
+    return render_template('clusters.html',
+        offset=offset, limit=limit, batches=batches,
+        ordered_list=clusters_id_sorted[offset:offset+limit+1],
+        idx_to_path=idx_to_path,
+        clusters=clusters)
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser("Visualize Clusters")
+    parser.add_argument("--lfw_dir", required=True, type=str)
+    parser.add_argument("--clusters", default="data/clusters.json", type=str)
+    args = parser.parse_args()
+    idx_to_path = create_idx_to_path(args.lfw_dir)
+    clusters = json.load(open(args.clusters))
+    app.run()
